53c31a72b0056bbce04b0bedb30e8d6d859ed2ae
Gearen Liem J <GearenLiemJ@JohnDeere.com>
2020-02-26 13:24:48 -0600

    Completed first-recurring challenge

--- hash/first-recurring-character.js
+++ hash/first-recurring-character.js
@@ -7,3 +7,19 @@
  *
  * given and array = [2,3, 4, 5]
  */
+function firstRecurringCharacter(array) {
+  const obj = {};
+
+  for (let i = 0; i < array.length; i++) {
+    const item = array[i];
+    if (!obj[item]) {
+      obj[item] = i;
+    } else {
+      return item;
+    }
+  }
+
+  return undefined;
+} // O(n) time and space complexity
+let array = [2, 1, 1, 2, 3, 5, 1, 2, 4];
+console.log(firstRecurringCharacter(array));
